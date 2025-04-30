346745e4a9fc4b13685bac639bec1e2ded75b735
hsan-isetsf <hsan.isetsf@gmail.com>
2025-04-30 10:13:39 +0200

    commit-changes

--- routes/article.route.js
+++ routes/article.route.js
@@ -14,7 +14,7 @@ try {
 }
 })
 
-router.put("/:id",async(req,res)=>{
+router.put("/:id",async(req,res)=>{ console.log(req.body)
     try {
         const art1 = await Article.findByIdAndUpdate(
             req.params.id,
